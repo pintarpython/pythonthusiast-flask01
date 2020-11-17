6576bcaf5e74a7d1d34298345eb5549f9c6921c1
pram <pramudyowee@gmail.com>
2020-11-17 21:59:27 +0700

    First commit

---
+++ web_app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('settings.py')
+
+    @app.route('/')
+    def index():
+        return render_template('index.html', TITLE='Flask 01')
+
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html', TITLE='Flask 01')
+
+    return app
